102622293d0ceac52ea43f75baccdc5bfa7b6073
Omar Ouhra <omarmaroc26@gmail.com>
2021-08-16 12:23:13 +0100

    Unit Testing

---
+++ function.test.js
@@ -0,0 +1,45 @@
+const functions = require("./function");
+
+// toBe
+test("add 2+2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
+});
+
+// toBeNull
+test("should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+// toBeFalsy
+test("should be falsy", () => {
+  expect(functions.checkValue()).toBeFalsy();
+});
+
+// toEqual
+test("user should be Omar Ouhra", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Omar",
+    lastName: "Ouhra",
+  });
+});
+
+
+// Less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1+load2).toBeLessThan(1600)
+})
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+})
+
+
+// Arrays
+test('Admin should be in usernames', () => {
+    usernames=['omar','ali','mohamed']
+    expect(usernames).toContain('ali');
+})
+
